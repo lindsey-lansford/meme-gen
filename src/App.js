0b4d35109e4628617b447720ae103818ac48f0d1
Lindsey <lindsey.lansford@gmail.com>
2023-03-16 17:23:39 -0600

    imported search results

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SearchResultsContainer from './components/SearchResults.js'
+import SearchResults from './components/SearchResults.js'
 
 function App() {
   return (
-    <div className="content">
-      <SearchResultsContainer/>
+    <div>
+      <SearchResults/>
     </div>
   );
 }
