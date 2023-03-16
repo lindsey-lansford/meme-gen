d33e9a82a1bc0031dec1111fe651bcbdd2686d28
Lindsey <lindsey.lansford@gmail.com>
2023-03-16 15:27:22 -0600

    formatting

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,11 @@
 // Axios is a popular NPM package used for preforming API requests
-import axios from 'axios';
+import axios from "axios";
 
 // Using axios, we create a search method that is specific to our use case and export it at the bottom
-const search = async (query) =>
-  axios.get(`https://api.giphy.com/v1/gifs/search?q=${query}$&api_key=u8nFOzZbIweHCO9V4SS4vR9WLuzLQDLo`);
+const search = (query) =>
+  axios.get(
+    `https://api.giphy.com/v1/gifs/search?q=${query}$&api_key=u8nFOzZbIweHCO9V4SS4vR9WLuzLQDLo`
+  );
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
-export default {search};
+export default { search };
