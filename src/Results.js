5f4df155da3a3671ce77029f1932129af0ff86f9
Lindsey <lindsey.lansford@gmail.com>
2023-03-16 14:43:41 -0600

    renaming file

---
+++ src/Results.js
@@ -0,0 +1,11 @@
+// Import our search method
+import search from '../utils/API';
+import { useState, useEffect } from 'react';
+
+const Results = () => {
+  return (
+    <div>Card</div>
+  )
+};
+
+export default Results;
