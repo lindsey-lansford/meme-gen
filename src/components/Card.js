b3a52bb9b7eb9eee732eec926958fcc05c7f4c7d
Lindsey <lindsey.lansford@gmail.com>
2023-03-16 15:27:42 -0600

    adding card file

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+
+const Card = (props) => {
+  return (
+    <div className="card text-center">
+      <div className="card-header">
+        <h2>{props.heading}</h2>
+      </div>
+      <div className="card-body">{props.children}</div>
+    </div>
+  );
+}
+
+export default Card;
