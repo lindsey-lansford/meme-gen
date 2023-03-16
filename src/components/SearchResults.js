90919b33ef399e48cef558d3e5d781ede5688ada
Lindsey <lindsey.lansford@gmail.com>
2023-03-16 17:30:46 -0600

    reformatting

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,11 +1,27 @@
-// Import our search method
-import { useState, useEffect } from 'react';
-import API from '../utils/API';
+import { useState, useEffect } from "react";
 
-const SearchResultsContainer = () => {
-  return (
-    <div>Card</div>
-  )
+import GifDetail from "./GifDetail";
+
+// Import our search method
+import search from "../utils/API";
+
+const SearchResults = () => {
+  // Declare a new state variable, "results"
+  const [results, setResults] = useState([]);
+
+  // Method to get search results and set state
+  const searchGiphy = async (query) => {
+    const response = await search(query);
+    setResults(response.data.data);
+  };
+
+  // We want to run this method when the component first loads so that we have images of 'x category' to display
+  // The second argument is the dependency array. This means that this method will only run when the component first loads
+  useEffect(() => {
+    searchGiphy("crazy people");
+  }, []);
+
+  return <GifDetail results={results} />;
 };
 
-export default SearchResultsContainer;
+export default SearchResults;
