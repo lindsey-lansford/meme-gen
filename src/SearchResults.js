b9c3590ab8f6e2a1861c9dc820f778c36618123c
Lindsey <lindsey.lansford@gmail.com>
2023-03-16 14:44:58 -0600

    renaming again

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -2,10 +2,10 @@
 import search from '../utils/API';
 import { useState, useEffect } from 'react';
 
-const Results = () => {
+const SearchResults = () => {
   return (
     <div>Card</div>
   )
 };
 
-export default Results;
+export default SearchResults;
